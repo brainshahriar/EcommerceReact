9866a806fec7e3abd1c182fe8fa6d39aeda86d83
brain <shahriarmehedi95@gmail.com>
2021-11-09 21:34:12 +0600

    dom

--- src/components/Classs.js
+++ src/components/Classs.js
@@ -14,14 +14,24 @@ class Learning extends Component{
         };
     };
 ClickHandler = () => {
-    console.log("Hwllo shahriar");
-    alert('clicked');
+    // console.log("Hello shahriar");
+    // alert('clicked');
+    this.setState({
+        fruits:[
+            {name:'ahsan',weight:'20'},
+            {name:'habib',weight:'21'},
+            {name:'lelin',weight:'22'}
+        ]
+    });
 };
     render(){
         return (
             <div>
                 <button onClick={this.ClickHandler}>Click ME </button>
-                <h1>babu name is {this.state.fruits[1].name} and weight is {this.state.fruits[2].weight}</h1>
+                <h1>babu name is {this.state.fruits[0].name} and weight is {this.state.fruits[0].weight}</h1>
+                <h1>babu name is {this.state.fruits[1].name} and weight is {this.state.fruits[1].weight}</h1>
+                <h1>babu name is {this.state.fruits[2].name} and weight is {this.state.fruits[2].weight}</h1>
+
             </div>
         );
     }
