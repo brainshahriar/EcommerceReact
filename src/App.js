922827acf819489773c3fb7f4777ba378620c7ad
brain <shahriarmehedi95@gmail.com>
2021-11-09 23:34:40 +0600

    onChange

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 class App extends React.Component{
+
+  state={
+    name:""
+  }
+
+  changeData =(e)=>{
+    let val =e.target.value;
+    this.setState({name:val})
+  }
   render(){
     return(
       <div className="App">
-        <h1>HEllos</h1>
+        <h1>HEllos {this.state.name}</h1>
         <div>
           <form>
-            <div className="form-group">
+            <div className="form-group" style={{ marginLeft:200 }}>
                 <label>Username : </label>
                 <br></br>
-                  <input type="text" name="username"></input>
+                  <input type="text" name="username" onChange={this.changeData}></input>
                   <button type="submit">Submit</button>
             </div>
           </form>
