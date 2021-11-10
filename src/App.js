bedee38ae86ad4ff4ce2070d3bad8957950f818b
brain <shahriarmehedi95@gmail.com>
2021-11-10 17:32:54 +0600

    lists

--- src/App.js
+++ src/App.js
@@ -2,33 +2,21 @@ import React from 'react';
 
 class App extends React.Component{
 
-  state={
-    username:""
-  }
 
-  changeData =(e)=>{
-    let val =e.target.name;  //username
-    let myvalue=e.target.value //geting value
-    this.setState({[val]:myvalue}); //value set on the state
-  }
 
-  formSubmit = (e) => {
-    e.preventDefault();
-    console.log(this.state.username);
-  }
+
   render(){
+
+    const names = ['a','b','c','d','e'];
+    const items = names.map((item,idx)=>{
+      return <li key={idx}>{idx}:{item}</li>
+    });
+
     return(
       <div className="App">
-        <h1>Hello</h1>
+        <h1>Student List</h1>
         <div>
-          <form onSubmit={this.formSubmit}>
-            <div className="form-group" style={{ marginLeft:200 }}>
-                <label>Username : </label>
-                <br></br>
-                  <input type="text" name="username" onChange={this.changeData}></input>
-                  <button type="submit">Submit</button>
-            </div>
-          </form>
+          {items}
         </div>
       </div>
     )
